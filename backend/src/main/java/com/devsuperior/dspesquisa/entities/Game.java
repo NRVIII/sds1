b6591b049c1479b57f7c680ac77789d0be02cd31
Diogo Lopes Pereira <nrviii@outlook.com>
2020-09-15 16:45:45 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,101 @@
+package com.devsuperior.dspesquisa.entities;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game implements Serializable {
+    private static final long serialVersionUID = -766966529394380010L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Platform platform;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre  genre;
+
+    @OneToMany(mappedBy = "game")
+    private List<Record> records = new ArrayList<>();
+
+    // === CONSTRUCTORS ===
+    public Game() {
+    }
+
+    public Game(Long id, String title, Platform platform,
+        Genre genre) {
+        this.id = id;
+        this.title = title;
+        this.platform = platform;
+        this.genre = genre;
+    }
+
+    // === GETTERS & SETTERS ===
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Platform platform) {
+        this.platform = platform;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    // === HASH E EQUALS() ===
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Game game = (Game) o;
+        return id.equals(game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
