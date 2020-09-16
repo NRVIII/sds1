79bb424b1f5d2851df419bdc22b640ef063b81fc
Diogo Lopes Pereira <nrviii@outlook.com>
2020-09-16 14:46:58 -0300

    Home Page Created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from  './components/Header/index'
+import Header from './components/Header'
+import Home from "./pages/Home";
 
 // JSX / TSX
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   );
 }
---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {ReactComponent as ArrowIcon} from "../../assets/arrow.svg";
+import {ReactComponent as GamerImage} from "../../assets/gamer.svg";
+import './styles.css'
+
+const Home = () => (
+  // MAIN TEXT FROM HOME
+  <div className="home-container">
+    <div className="home-text">
+
+      {/* TITLE */}
+      <h1 className="home-text-title">
+        Quais jogos a galera gosta mais?
+      </h1>
+
+      {/* DESCRIPTION */}
+      <h3 className="home-text-subtitle">
+        Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
+      </h3>
+
+      {/* MAIN BUTTON */}
+      <div className="home-actions">
+
+        {/* BUTTON */}
+        <button className="home-btn">
+          QUERO SABER QUAIS SÃO
+        </button>
+
+        {/* ICON */}
+        <div className="home-btn-icon">
+          <ArrowIcon/>
+        </div>
+      </div>
+    </div>
+
+    <GamerImage className="home-image"/>
+
+  </div>
+);
+
+export default Home;
